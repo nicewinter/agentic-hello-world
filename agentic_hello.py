674ea86b7185358e139449bcee12360cc61ad5db
nicewinter <nicewinter68@hotmail.com>
2026-04-17 16:32:33 +0100

    Add agentic_hello.py script

---
+++ agentic_hello.py
@@ -0,0 +1,10 @@
+"""A simple script that prints a greeting message."""
+
+
+def main():
+    """Print a greeting message."""
+    print("Hello Agentic World")
+
+
+if __name__ == "__main__":
+    main()
